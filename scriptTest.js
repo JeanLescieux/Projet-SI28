a34db68733a50de9083146d5890eb3e6e28f4cf5
@jlescieu <jean.lescieu@etu.utc.fr>
2024-03-24 16:44:06 +0100

    fenêtre fonctionelle

--- scriptTest.js
+++ scriptTest.js
@@ -1,9 +1,23 @@
-function openFileExplorer() {
-  var modal = document.getElementById("fenetreModale");
-  modal.style.display = "block"; // Affiche la fenêtre modale
+async function openFileExplorer() {
+  // Récupère l'élément modalContent
+  var modalContent = document.getElementById('modalContent');
+  try {
+      // Charge le contenu de fileExplorer.html dans modalContent
+      const response = await fetch('fileExplorer.html');
+      if (!response.ok) {
+          throw new Error('Erreur lors du chargement de fileExplorer.html');
+      }
+      modalContent.innerHTML = await response.text();
+      // Affiche la fenêtre modale en ajoutant la classe "show"
+      document.getElementById('fenetreModale').style.display = 'block';
+      console.log('Fenêtre modale affichée');
+  } catch (error) {
+      console.error(error);
+  }
 }
 
 function closeFileExplorer() {
-  var modal = document.getElementById("fenetreModale");
-  modal.style.display = "none"; // Cache la fenêtre modale
+  // Cache la fenêtre modale en modifiant directement le style pour masquer l'élément
+  document.getElementById('fenetreModale').style.display = 'none';
+  console.log('Fenêtre modale cachée');
 }
