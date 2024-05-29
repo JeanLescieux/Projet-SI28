7ea89fac61a0226db830646bae953988e4c9bc50
cgommard <chloe.gommard-dauba@etu.utc.fr>
2024-05-29 23:34:48 +0200

    admin finit

---
+++ projectFiles/Game/Apps/Internet/webPage/adminConnection.js
@@ -0,0 +1,25 @@
+function openTab(tabName) {
+    // Hide all elements with class="tab-content"
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("taba");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "";
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    if(tabName === 'main') {
+        tablinks[0].style.backgroundColor = "rgb(0, 0, 0)";
+        tablinks[0].style.color = "rgb(0, 255, 0)";
+    } else if(tabName === 'admin') {
+        tablinks[1].style.backgroundColor = "rgb(0, 255, 0)";
+        tablinks[1].style.color = "rgb(0, 0, 0)";
+    }
+}
